0b114429c7af3b3184e599d88c033319829b7971
ahmed hassan <uyu365222@gmail.com>
2024-02-14 20:34:12 +0200

    ccc

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask,render_template,redirect,Request
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+# create form class
+app.config['SECRET_KEY'] = "my super secret key"
+
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name ",validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
     return(render_template('index.html'))
 
-@app.route('/user/<name>')
-def user(name):
-    return "<h1>hello {}</h1>".format(name)
+@app.route('/name',methods=['GET','POST'])
+def user():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return(render_template('name.html',form=form,name=name))
 
 
 if __name__ == "__main__":
